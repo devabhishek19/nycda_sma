ee9101ea11a996004ac4ba7b3ec13b6547745799
Abhishek Patel <dev.abhishek19@gmail.com>
2015-06-18 21:34:05 -0400

    added remaining work

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,32 +7,55 @@ class StudentsController < ApplicationController
   	@student = Student.find_by_id(params[:id])
   end
 
-  def edit
-  	@student = Student.find_by_id(params[:id])
-  end
-
-  def update
-  	@student = Student.find_by_id(params[:id])
-  	student.update_attributes(student_params)
-  end
-
   def new
   	@student = Student.new
   end
 
   def create
+    #initialize a new student object with form values
   	@student = Student.new(student_params)
-  	@student.save
-  	redirect_to students_path
+    #if student sucessfully created redirect to the index view
+    #and then display a flash message
+    if @student.save
+      flash[:notice] = "Student was Sucsessfully added!"
+  	 redirect_to student_path(@student)
+    else
+      flash[:alert] = "Student was not able to add!"
+      render :new
+    end
   end
 
-  def delete
-  	@student = Student.find_by_id(params[:id])
-  	@student.destroy
-  	redirect_to students_path
+   def edit
+    @student = Student.find_by_id(params[:id])
   end
+
+  def update
+    @student = Student.find_by_id(params[:id])
+    if 
+      @student.update_attributes(student_params)
+      flash[:notice] = "Student was Sucsessfully Edited!"
+      redirect_to students_path
+    else
+      flash[:alert] = "Student was not able to Edit!"
+      render :edit
+    end 
+  end
+
+  def destroy
+  	@student = Student.find_by_id(params[:id])
+  	if @student.destroy
+      flash[:notice] = "Student was Sucsessfully Remove!"
+  	redirect_to students_path, flash = {sucsess: "Student sucsessfully added!"}
+    end
+  end
+
+  private
 
   def student_params
-  	params.require(:student).permit(:fname, :lname, :email, :phone, :status)
+  	params.require(:student).permit(:fname, :lname, :email, :phone)
   end
+
+  
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ActiveRecord::Base
+  validates :email, presence: true, uniqueness: true
 end
